2b393bf4167b39fd273a0e1b18b6f676ae95e57d
Pranav <pranavbairy2@gmail.com>
2025-03-18 12:59:38 +0530

    updated pictures

--- app.py
+++ app.py
@@ -6,3 +6,24 @@ app = Flask(__name__)
 def hello_world():
     return render_template('home.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+#create for render login.html
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+#create for render register.html
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
